ab95bb5656da28b353ad6e43f16299b0a0dc955d
Admin <brtgfc@bk.ru>
2021-10-17 17:55:24 +0300

    python not working, java working

--- app/src/main/java/com/example/opiscoursework/MainViewModel.kt
+++ app/src/main/java/com/example/opiscoursework/MainViewModel.kt
@@ -31,28 +31,39 @@ class MainViewModel : ViewModel() {
                 Imgproc.cvtColor(src, gray, Imgproc.COLOR_BGR2GRAY)
 
 //                Core.inRange(src, Scalar(0.0, 0.0, 0.0), Scalar(0.0, 0.0, 0.0), gray)
-                Imgproc.blur(src, gray, Size(3.0, 3.0))
-                Imgproc.Canny(src, gray, 50.0, 150.0, 3, false)
+//                Imgproc.blur(src, gray, Size(3.0, 3.0))
+                Imgproc.Canny(gray, gray, 10.0, 250.0)
+                val kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, Size(7.0, 7.0))
+                Imgproc.morphologyEx(gray, gray, Imgproc.MORPH_CLOSE, kernel)
+//                Imgproc.Canny(src, gray, 50.0, 150.0, 3, false)
                 val contours: List<MatOfPoint> = ArrayList()
                 val hierarchy = Mat()
                 Imgproc.findContours(
                     gray,
                     contours,
                     hierarchy,
-                    Imgproc.RETR_TREE,
+                    Imgproc.RETR_EXTERNAL,
                     Imgproc.CHAIN_APPROX_SIMPLE
                 )
                 for (contourIdx in contours.indices) {
-                    Imgproc.drawContours(
-                        src,
-                        contours,
-                        contourIdx,
-                        Scalar(255.0),
-                        1,
-                        Imgproc.LINE_AA,
-                        hierarchy,
-                        0
-                    )
+                    val matOfPoint = contours[contourIdx]
+                    val tempMat = MatOfPoint2f()
+                    matOfPoint.convertTo(tempMat, CvType.CV_32F)
+
+                    val sm = Imgproc.arcLength(tempMat, true)
+                    val apd = MatOfPoint2f()
+                    Imgproc.approxPolyDP(tempMat, apd, 0.02 * sm, true)
+                    if (apd.elemSize() > 2)
+                        Imgproc.drawContours(
+                            src,
+                            contours,
+                            contourIdx,
+                            Scalar(255.0),
+                            1,
+                            Imgproc.LINE_AA,
+                            hierarchy,
+                            0
+                        )
                 }
                 val result = Bitmap.createBitmap(it.width, it.height, it.config)
                 Utils.matToBitmap(src, result)
